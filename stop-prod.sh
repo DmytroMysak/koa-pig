32c15315a3fa135a2de8b3e2299d97d3f2746542
Dmytro Mysak <dmytro.mysak@gmail.com>
2018-09-07 09:51:36 +0300

    fix stop command

--- stop-prod.sh
+++ stop-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pm2 ./node_modules/.bin/pm2/stop bot-prod
-pm2 ./node_modules/.bin/pm2/delete bot-prod
+./node_modules/.bin/pm2 stop bot-prod
+./node_modules/.bin/pm2 delete bot-prod
