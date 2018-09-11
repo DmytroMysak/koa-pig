83b7b510d4cd0a5f640c31fabc5b248370705e96
Dmytro Mysak <dmytro.mysak@gmail.com>
2018-09-11 23:13:25 +0300

    fix volume

--- businessLogic/audioBL.js
+++ businessLogic/audioBL.js
@@ -80,8 +80,7 @@ export default class Audio {
   playSong(audioData) {
     const fullPath = this.getFullPathToFile(audioData.fileName);
     return new Promise((resolve, reject) => {
-      this.currentPlayer = this.player.play(fullPath, { mplayer: [`volume=${audioData.volume || -1}`] }, (err) => {
-      // this.currentPlayer = this.player.play(fullPath, (err) => {
+      this.currentPlayer = this.player.play(fullPath, { mplayer: ['-volume', audioData.volume || -1] }, (err) => {
         if (!err || err === 1) {
           // err === 1 ==> force stop player
           this.currentPlayer = null;
