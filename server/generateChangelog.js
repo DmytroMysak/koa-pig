2cddf3da02361a045d0170613b5b6d73e473a74c
Dmytro Mysak <dmytro.mysak@eleks.com>
2020-10-16 18:43:29 +0300

    docs(changelog): generate init changelog

--- server/generateChangelog.js
+++ server/generateChangelog.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const fs = require('fs');
 const conventionalChangelog = require('conventional-changelog');
 const config = require('cz-conventional-changelog');
 
-conventionalChangelog({ config }).pipe(process.stdout); // or any writable stream
+conventionalChangelog({ config, releaseCount: 0 }).pipe(fs.createWriteStream('./CHANGELOG.md'));
