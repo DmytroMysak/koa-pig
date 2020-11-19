62080db95f551fee65a0b56d369ac73864952c20
Dmytro Mysak <dmytro.mysak@eleks.com>
2020-11-20 00:50:25 +0200

    feat(client/server): added stop all command

--- client/src/services/commands/stopAllCommand.js
+++ client/src/services/commands/stopAllCommand.js
@@ -3,12 +3,11 @@ const BaseCommand = require('./baseCommand');
 module.exports = class StopCommand extends BaseCommand {
   constructor(channel) {
     super(channel);
-    this.name = 'stop-all-songs';
+    this.name = 'stop-song';
   }
 
   // eslint-disable-next-line no-unused-vars
   async execute(message) {
-    this.player.clearQueue();
     this.player.stopSong();
     this.ack();
   }
---
+++ server/src/services/commands/stopAllCommand.js
@@ -0,0 +1,16 @@
+const BaseCommand = require('./baseCommand');
+const ClientService = require('../clientService');
+
+module.exports = class StopAudioCommand extends BaseCommand {
+  constructor() {
+    super();
+    this.name = ['stopAll'];
+    this.type = 'command';
+  }
+
+  async execute(ctx) {
+    super.execute(ctx);
+
+    return ClientService.sendToClients({ command: 'stop-all-songs' }, ctx.user.selectedClients);
+  }
+};
